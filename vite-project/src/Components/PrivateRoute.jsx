4b910517c8919463c1fe3c83789443c8b7f0e64c
Pablo-Mateus <pablo17mateus@hotmail.com>
2025-06-20 02:41:21 -0300

    implementando PrivateRoute

--- vite-project/src/Components/PrivateRoute.jsx
+++ vite-project/src/Components/PrivateRoute.jsx
@@ -2,6 +2,32 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  React.useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/check-auth", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setIsAuthenticated(data.isAuthenticated);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (err) {
+        console.log("Erro ao verificar autenticação", err);
+      } finally {
+        setIsAuthenticated(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
