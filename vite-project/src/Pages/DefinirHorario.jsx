a8b06c591dccfa586b7d99419f305c04c36557b4
Pablo-Mateus <pablo17mateus@hotmail.com>
2025-06-26 22:35:55 -0300

    Time picker

--- vite-project/src/Pages/DefinirHorario.jsx
+++ vite-project/src/Pages/DefinirHorario.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
 const DefinirHorario = () => {
   return (
     <>
-      <input type="time" name="" id="" />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["TimePicker"]}>
+          <TimePicker label="Basic time picker" />
+        </DemoContainer>
+      </LocalizationProvider>
     </>
   );
 };
